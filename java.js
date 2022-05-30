581878b168ee33b2e7198e5b7097a5095684d395
Eric <eric.annacone@gmail.com>
2022-05-30 12:02:35 -0600

    Begin js work and connecting front to backendfunctionality

---
+++ java.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
+
+
+
